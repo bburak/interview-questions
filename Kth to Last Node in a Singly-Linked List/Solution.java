346da923af24bffeed5fdde5a94305485a048bb4
Burak BİLGİÇ <bburak@users.noreply.github.com>
2020-03-01 00:05:24 +0300

    Kth to Last Node in a Singly-Linked List
    You have a linked list ↴ and want to find the kkth to last node.
    
    Write a method kthToLastNode() that takes an integer kk and the headNode of a singly-linked list, and returns the kkth to last node in the list.

---
+++ Kth to Last Node in a Singly-Linked List/Solution.java
@@ -0,0 +1,124 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class Solution {
+
+    public static class LinkedListNode {
+
+        public int value;
+        public LinkedListNode next;
+
+        public LinkedListNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public static LinkedListNode kthToLastNode(int k, LinkedListNode head) {
+        
+        if (k < 1) {
+        throw new IllegalArgumentException(
+            "Impossible to find less than first to last node: " + k);
+    }
+
+    LinkedListNode leftNode  = head;
+    LinkedListNode rightNode = head;
+
+    // move rightNode to the kth node
+    for (int i = 0; i < k - 1; i++) {
+
+        // but along the way, if a rightNode doesn't have a next,
+        // then k is greater than the length of the list and there
+        // can't be a kth-to-last node! we'll raise an error
+        if (rightNode.next == null) {
+            throw new IllegalArgumentException(
+                "k is larger than the length of the linked list: " + k);
+        }
+
+        rightNode = rightNode.next;
+    }
+
+    // starting with leftNode on the head,
+    // move leftNode and rightNode down the list,
+    // maintaining a distance of k between them,
+    // until rightNode hits the end of the list
+    while (rightNode.next != null) {
+        leftNode  = leftNode.next;
+        rightNode = rightNode.next;
+    }
+
+    // since leftNode is k nodes behind rightNode,
+    // leftNode is now the kth to last node!
+    return leftNode;
+    }
+
+
+
+
+    // tests
+
+    @Test
+    public void firstToLastNodeTest() {
+        final LinkedListNode[] listNodes = valuesToLinkedListNodes(new int[] {1, 2, 3, 4});
+        final int k = 1;
+        final LinkedListNode actual = kthToLastNode(k, listNodes[0]);
+        final LinkedListNode expected = listNodes[listNodes.length - k];
+        assertSame(expected, actual);
+    }
+
+    @Test
+    public void secondToLastNodeTest() {
+        final LinkedListNode[] listNodes = valuesToLinkedListNodes(new int[] {1, 2, 3, 4});
+        final int k = 2;
+        final LinkedListNode actual = kthToLastNode(k, listNodes[0]);
+        final LinkedListNode expected = listNodes[listNodes.length - k];
+        assertSame(expected, actual);
+    }
+
+    @Test
+    public void firstNodeTest() {
+        final LinkedListNode[] listNodes = valuesToLinkedListNodes(new int[] {1, 2, 3, 4});
+        final int k = 4;
+        final LinkedListNode actual = kthToLastNode(k, listNodes[0]);
+        final LinkedListNode expected = listNodes[listNodes.length - k];
+        assertSame(expected, actual);
+    }
+
+    @Test(expected = Exception.class)
+    public void kIsGreaterThanLinkedListLengthTest() {
+        final LinkedListNode[] listNodes = valuesToLinkedListNodes(new int[] {1, 2, 3, 4});
+        final int k = 5;
+        kthToLastNode(k, listNodes[0]);
+    }
+
+    @Test(expected = Exception.class)
+    public void kIsZeroTest() {
+        final LinkedListNode[] listNodes = valuesToLinkedListNodes(new int[] {1, 2, 3, 4});
+        final int k = 0;
+        kthToLastNode(k, listNodes[0]);
+    }
+
+    private static LinkedListNode[] valuesToLinkedListNodes(int[] values) {
+        final LinkedListNode[] nodes = new LinkedListNode[values.length];
+        for (int i = 0; i < values.length; i++) {
+            nodes[i] = new LinkedListNode(values[i]);
+            if (i > 0) {
+                nodes[i - 1].next = nodes[i];
+            }
+        }
+        return nodes;
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
